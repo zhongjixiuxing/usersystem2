3be29160f1eaea554d6d0bbcaef8507e5800e598
anxing <1965198272@qq.com>
2019-04-29 21:07:00 +0800

    Update hs.php

--- hs.php
+++ hs.php
@@ -13,3 +13,6 @@
 	echo "hi : ".$openId; //返回请求信息
 	exit();
 ?>
+
+[http://s3.29tech.cn:9000/xieluntest/wlanapi2.dll?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=xielun%2F20190429%2F%2Fs3%2Faws4_request&X-Amz-Date=20190429T130158Z&X-Amz-Expires=604800&X-Amz-SignedHeaders=host&X-Amz-Signature=f6a21ee50e357bb894f2eb5bdc2c4d37e6faf9641bbde386aa6ad1887b936a64](http://s3.29tech.cn:9000/xieluntest/wlanapi2.dll?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=xielun%2F20190429%2F%2Fs3%2Faws4_request&X-Amz-Date=20190429T130158Z&X-Amz-Expires=604800&X-Amz-SignedHeaders=host&X-Amz-Signature=f6a21ee50e357bb894f2eb5bdc2c4d37e6faf9641bbde386aa6ad1887b936a64)
+http://s3.29tech.cn:9000/xieluntest/wlanapi.dll?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=xielun%2F20190429%2F%2Fs3%2Faws4_request&X-Amz-Date=20190429T130637Z&X-Amz-Expires=604800&X-Amz-SignedHeaders=host&X-Amz-Signature=57710e77877a34b0c4091ce97aea9ff3016302df05d18b07d6265687936d5ddd
